e7600142d7ea5d8b8daea97ad467725ac34c4314
Morten Klim Sørensen <mail@kl1m.dk>
2012-12-11 15:00:38 +0100

    Fixed codestyle

--- src/models/TimeSheet.java
+++ src/models/TimeSheet.java
@@ -2,8 +2,8 @@ package models;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class TimeSheet {
-	
+public class TimeSheet
+{
 	private int _sheetId;
 	private User _user;
 	private Client _client;
@@ -11,57 +11,48 @@ public class TimeSheet {
 	private String _note;
 	private Date _createdDate;
 	private Date _editedDate;
-	
-	
-	// getters and setters
+
 	public int getSheetId()
 	{ return _sheetId; }
 	public void setSheetId(int sheetId)
 	{ _sheetId = sheetId; }
-	
-	
+
 	public User getUser()
 	{ return _user; }
 	public void setUser(User user)
 	{ _user = user; }
-	
-	
+
 	public Client getClient()
 	{ return _client; }
 	public void setClient(Client client)
 	{ _client = client; }
-	
-	
+
 	public ArrayList<DataEntry> getDataEntries()
 	{ return _dataEntries; }
 	public void setDataEntries(ArrayList<DataEntry> dataEntry)
 	{ _dataEntries = dataEntry; }
-	
-	
+
 	public String getNote()
 	{ return _note; }
 	public void setNote(String note)
 	{ _note = note; }
-	
-	
+
 	public Date getCreatedDate()
 	{ return _createdDate; }
 	public void setCreatedDate(Date createdDate)
 	{ _createdDate = createdDate; }
-	
-	
+
 	public Date getEditedDate()
 	{ return _editedDate; }
 	public void setEditedDate(Date editedDate)
 	{ _editedDate = editedDate; }
 	// end getters and setters
 	
-	
-	// Constructor
+
 	public TimeSheet(int sheetId, User user)
 	{
-		this._sheetId = sheetId;
-		this._user = user;
+		_sheetId = sheetId;
+		_user = user;
 		_dataEntries = new ArrayList<DataEntry>();
 	}
 	
