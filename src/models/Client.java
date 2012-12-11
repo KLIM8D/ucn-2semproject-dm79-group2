83ecaf53c021f91389d6dd49a922ee396a8ecfe8
Chris <christind@hotmail.com>
2012-12-11 13:22:31 +0100

    Added Client to models

---
+++ src/models/Client.java
@@ -0,0 +1,69 @@
+package models;
+
+import java.util.Date;
+
+public class Client {
+
+	String _name;
+	String _address;
+	City _city;
+	long _phoneNo;
+	String _eMail;
+	Date _createdDate;
+	Date _editedDate;
+	
+	public String getName() 
+	{ return _name; }
+	
+	public void set_name(String name) 
+	{ _name = name; }
+	
+	public String getAddress() 
+	{ return _address; }
+	
+	public void setAddress(String address) 
+	{ _address = address; }
+	
+	public City getCity() 
+	{ return _city; }
+	
+	public void setCity(City city) 
+	{ _city = city; }
+	
+	public long getPhoneNo() 
+	{ return _phoneNo; }
+	
+	public void setPhoneNo(long phoneNo) 
+	{ _phoneNo = phoneNo; }
+	
+	public String getEmail() 
+	{ return _eMail; }
+	
+	public void setEmail(String eMail)
+	{ _eMail = eMail; }
+	
+	public Date getCreatedDate() 
+	{ return _createdDate; }
+	
+	public void setCreatedDate(Date createdDate) 
+	{ _createdDate = createdDate; }
+	
+	public Date getEditedDate() 
+	{ return _editedDate; }
+	
+	public void setEditedDate(Date editedDate) 
+	{ _editedDate = editedDate; }
+
+	public Client(String name, String address, City city, long phoneNo, String eMail, Date createdDate, Date editedDate) 
+	{
+		//super();
+		_name = name;
+		_address = address;
+		_city = city;
+		_phoneNo = phoneNo;
+		_eMail = eMail;
+		_createdDate = createdDate;
+		_editedDate = editedDate;
+	}
+	
+}
