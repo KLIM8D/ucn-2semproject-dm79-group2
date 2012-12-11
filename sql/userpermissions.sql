11ebfa7e8263e1c43f460e72cf6a91abbbae3db1
iammadsnielsen <contact@madsnielsen.net>
2012-12-11 11:00:35 +0100

    Even more brainfarts fixed...

--- sql/userpermissions.sql
+++ sql/userpermissions.sql
@@ -1,9 +1,9 @@
-INSERT INTO [dbo].[UserPermissions]([permissionId], [userRole], [creationDate], [editedDate]) 
+INSERT INTO [dbo].[UserPermissions]([userRole], [creationDate], [editedDate]) 
 	VALUES('Administrator', '2012-12-11 10:50:15', '2012-12-11 10:50:15')
 GO
-INSERT INTO [dbo].[UserPermissions]([permissionId], [userRole], [creationDate], [editedDate]) 
+INSERT INTO [dbo].[UserPermissions]([userRole], [creationDate], [editedDate]) 
 	VALUES('Superuser', '2012-12-11 10:50:15', '2012-12-11 10:50:15')
 GO
-INSERT INTO [dbo].[UserPermissions]([permissionId], [userRole], [creationDate], [editedDate]) 
+INSERT INTO [dbo].[UserPermissions]([userRole], [creationDate], [editedDate]) 
 	VALUES('User', '2012-12-11 10:50:15', '2012-12-11 10:50:15')
 GO
