22d08046ecaf89c1dc7fe2945f37e1daadd0b210
iammadsnielsen <contact@madsnielsen.net>
2012-12-11 13:57:42 +0100

    Added UserPermissions

---
+++ src/models/UserPermission.java
@@ -0,0 +1,52 @@
+/**
+ * filename    : User.java
+ * created     : Dec 11, 2012 (1:33:02 PM)
+ * description :
+ * -------------------------------------------------------
+ * @version    : 0.1
+ * @changes    :
+ */
+
+package models;
+
+import java.util.Date;
+
+public class UserPermission
+{
+	private int _permissionId;
+	private String _userRole;
+	private Date _createdDate;
+	private Date _editedDate;
+	
+	public int getPermissionId()
+	{ return _permissionId; }
+	
+	public void setPermissionId(int value)
+	{ _permissionId = value; }
+	
+	public String getUserRole()
+	{ return _userRole; }
+
+	public void setUserRole(String value)
+	{ this._userRole = value; }
+
+	public Date getCreatedDate()
+	{ return _createdDate; }
+
+	public void setCreatedDate(Date value)
+	{ _createdDate = value; }
+
+	public Date getEditedDate()
+	{ return _editedDate; }
+
+	public void setEditedDate(Date value)
+	{ _editedDate = value; }
+
+	public UserPermission(int permissionId, String userRole, Date createdDate, Date editedDate)
+	{
+		_permissionId = permissionId;
+		_userRole = userRole;
+		_createdDate = createdDate;
+		_editedDate = editedDate;
+	}	
+}
