64621427b2287e1db27d6eee25d5a880dbcc8c31
iammadsnielsen <contact@madsnielsen.net>
2012-12-12 12:43:42 +0100

    Added controllers for User and UserPermission

---
+++ src/controllers/UserCtrl.java
@@ -0,0 +1,52 @@
+/**
+ * filename    : UserCtrl.java
+ * created     : Dec 12, 2012 (12:34:32 PM)
+ * description :
+ * -------------------------------------------------------
+ * @version    : 0.1
+ * @changes    :
+ */
+
+package controllers;
+
+import models.User;
+import db.DBUser;
+import java.util.ArrayList;
+
+public class UserCtrl
+{
+	public UserCtrl()
+	{
+		
+	}
+	
+	public ArrayList<User> getAllUsers() throws Exception
+	{
+		DBUser dbu = new DBUser();
+		return dbu.getAllUsers();
+	}
+	
+	public User getUserById(int value) throws Exception
+	{
+		DBUser dbu = new DBUser();
+		return dbu.getUserById(value);
+	}
+	
+	public int insertUser(User user) throws Exception
+	{
+		DBUser dbu = new DBUser();
+		return dbu.insertUser(user);
+	}
+	
+	public int updateUser(User user) throws Exception
+	{
+		DBUser dbu = new DBUser();
+		return dbu.updateUser(user);
+	}
+	
+	public int deleteUser(User user) throws Exception
+	{
+		DBUser dbu = new DBUser();
+		return dbu.deleteUser(user);
+	}
+}
---
+++ src/controllers/UserPermissionCtrl.java
@@ -0,0 +1,52 @@
+/**
+ * filename    : UserPermissionCtrl.java
+ * created     : Dec 12, 2012 (12:17:18 PM)
+ * description :
+ * -------------------------------------------------------
+ * @version    : 0.1
+ * @changes    :
+ */
+
+package controllers;
+
+import models.UserPermission;
+import db.DBUserPermission;
+import java.util.ArrayList;
+
+public class UserPermissionCtrl
+{
+	public UserPermissionCtrl()
+	{
+		
+	}
+	
+	public ArrayList<UserPermission> getAllRoles() throws Exception
+	{
+		DBUserPermission dbup = new DBUserPermission();
+		return dbup.getAllRoles();
+	}
+	
+	public UserPermission getRoleById(int value) throws Exception
+	{
+		DBUserPermission dbup = new DBUserPermission();
+		return dbup.getRoleById(value);
+	}
+	
+	public int insertRole(UserPermission userPermission) throws Exception
+	{
+		DBUserPermission dbup = new DBUserPermission();
+		return dbup.insertRole(userPermission);
+	}
+	
+	public int updateRole(UserPermission userPermission) throws Exception
+	{
+		DBUserPermission dbup = new DBUserPermission();
+		return dbup.updateRole(userPermission);
+	}
+	
+	public int deleteRole(UserPermission userPermission) throws Exception
+	{
+		DBUserPermission dbup = new DBUserPermission();
+		return dbup.deleteRole(userPermission);
+	}
+}
