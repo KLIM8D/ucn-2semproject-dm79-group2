e11bd5704b14c1f7e80bcf6359be6d8c575c347f
iammadsnielsen <contact@madsnielsen.net>
2012-12-13 13:09:00 +0100

    Added application executer...

---
+++ src/Execute.java
@@ -0,0 +1,25 @@
+/**
+ * filename    : Execute.java
+ * created     : Dec 13, 2012 (1:08:08 PM)
+ * description :
+ * -------------------------------------------------------
+ * @version    : 0.1
+ * @changes    :
+ */
+
+import views.SystemUI;
+
+final class Execute
+{
+    public static void main(String[] args)
+    {
+    	try
+        {
+
+    	}
+    	catch(Exception e)
+    	{
+    		e.printStackTrace();
+    	}
+    }
+}
