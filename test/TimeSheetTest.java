630a7ee6d53337b9f5f1791e5a060875078f71da
RasmusDM79 <rlillienskjold@gmail.com>
2012-12-20 14:01:49 +0100

    TimeSheetTest added

---
+++ test/TimeSheetTest.java
@@ -0,0 +1,115 @@
+import db.DataAccess;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+import models.TimeSheet;
+import models.Client;
+import models.User;
+
+import controllers.TimeSheetCtrl;
+import controllers.ClientCtrl;
+import controllers.UserCtrl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created: 15-12-2012
+ * @version: 0.1
+ * Filename: ClientTest.java
+ * Description:
+ * @changes
+ */
+
+public class TimeSheetTest
+{
+    private TimeSheetCtrl _timeSheetCrtl;
+    private UserCtrl _userCtrl;
+    private ClientCtrl _clientCtrl;
+    
+    public TimeSheetTest()
+    {
+    }
+
+    @Before
+    public void setUp()
+    {
+    	_timeSheetCrtl = new TimeSheetCtrl();
+    	_userCtrl = new UserCtrl();
+    	_clientCtrl = new ClientCtrl();
+    }
+
+    @Test
+    public void insertTimeSheet() throws Exception
+    {
+        Calendar cal = Calendar.getInstance();
+        User newUser = _userCtrl.getUserById(1);
+        Client newClient = _clientCtrl.getClientById(1);
+                                 
+        TimeSheet newTimeSheet = new TimeSheet("L-445521", newUser, newClient ,"Der skal flere noter på", cal.getTime(), cal.getTime());
+        int rowsAffected = _timeSheetCrtl.insertTimeSheet(newTimeSheet);
+
+        assertEquals(1, rowsAffected);
+    }
+
+    @Test
+    public void getTimeSheetBySheetId() throws Exception
+    {
+        TimeSheet timeSheet = _timeSheetCrtl.getTimeSheetById(1);
+
+        assertNotNull(timeSheet);
+    }
+    
+    @Test
+    public void getTimeSheetByCaseId() throws Exception
+    {
+        TimeSheet timeSheet = _timeSheetCrtl.getTimeSheetByCaseId(1);
+
+        assertNotNull(timeSheet);
+    }
+    
+    @Test
+    public void getTimeSheetsByUser() throws Exception
+    {
+    	User user = _userCtrl.getUserById(1);
+    	ArrayList<TimeSheet> timeSheets;
+    	timeSheets = _timeSheetCrtl.getAllTimeSheetsByUser(user);
+    }
+    
+    @Test
+    public void getTimeSheetsByClient() throws Exception
+    {
+    	Client client = _clientCtrl.getClientById(1);
+    	ArrayList<TimeSheet> timeSheets;
+    	timeSheets = _timeSheetCrtl.getAllTimeSheetsByUser(client);
+    }
+       
+    @Test
+    public void deleteTimeSheet() throws Exception
+    {
+        DataAccess _da = DataAccess.getInstance();
+        long id = _da.getNextId("TimeSheets");
+        TimeSheet timeSheet = _timeSheetCrtl.getTimeSheetByCaseId((int)id - 1);
+        int rowsAffected = _timeSheetCrtl.deleteTimeSheet(timeSheet);
+
+        assertEquals(1, rowsAffected);
+    }
+    
+    @Test
+    public void updateTimeSheet() throws Exception
+    {
+        DataAccess _da = DataAccess.getInstance();
+        long id = _da.getNextId("TimeSheets");
+        TimeSheet timeSheet = _timeSheetCrtl.getTimeSheetByCaseId((int)id - 1);
+        client.setName("Bente Nielsen");
+        int rowsAffected = _timeSheetCrtl.updateTimeSheet(timeSheet);
+
+        assertEquals(1, rowsAffected);
+    }
+}
